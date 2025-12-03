ed9e96c5cadc17a15210a3d57224d24829b4343f
amorch-5 <124015019+amorch-5@users.noreply.github.com>
2025-12-03 21:53:51 +0000

    init

---
+++ nao_tts.py
@@ -0,0 +1,110 @@
+
+import time
+from naoqi import ALProxy
+import qi
+import math
+import random
+
+
+tts = ALProxy
+
+ip = "10.60.218.114"
+
+# text to speech proxy
+tts = ALProxy("ALTextToSpeech",ip , 9559)
+
+# mood proxy
+mood = ALProxy("ALMood", ip, 9559)
+session = qi.Session()
+session.connect("tcp://" + ip + ":9559")
+mood = session.service("ALMood")
+
+# animated speech proxy
+animated_speech = ALProxy("ALAnimatedSpeech",ip, 9559)
+
+# posture proxy
+posture_proxy = ALProxy("ALRobotPosture", ip, 9559)
+
+current_posture = posture_proxy.getPosture()
+
+text_old = ""
+
+# posture_proxy.goToPosture("StandInit", 1.0)
+
+# tracker proxy
+tracker = ALProxy("ALTracker", ip, 9559)
+frame = 2
+speed = 0.5
+use_whole_body = False
+# target_position = [1.0, 0.0, 0.0]
+# tracker.lookAt(target_position, frame, speed, use_whole_body)
+
+# # Sound function
+# def sound_located_callback(key, value):
+#     azimuth = value[1][0]
+#     elevation = value[1][1]
+#     print(azimuth, elevation)
+
+# Memory proxy
+memory = ALProxy("ALMemory", ip, 9559)
+soundLoc = ALProxy("ALSoundLocalization", ip, 9559)
+soundLoc.subscribe("SoundLocated")
+
+
+
+# if current_posture != "Stand":
+#     # Make NAO stand up
+#     posture_proxy.goToPosture("StandInit", 1.0)
+
+with open("listen.txt", "w") as f:
+    f.write("no")
+
+with open("response.txt", "w") as f:
+    f.write(" ")
+
+while True:
+    try:
+        data = memory.getData("ALSoundLocalization/SoundLocated")
+        azimuth = data[1][0]
+        elevation = data[1][1]
+        confidence = data[1][2]
+
+        sound_x = math.cos(elevation) * math.cos(azimuth)
+        sound_y = math.cos(elevation) * math.sin(azimuth)
+        sound_z = math.sin(elevation) + 1
+
+        # personmood = mood.currentPersonState()['smile']
+        # print("Current person state: ", personmood)
+
+        with open("response.txt", "r") as f:
+            text = f.read().replace('\n', ' ')
+
+
+        # random_look = random.randint(1,100)
+        # if random_look > 90:
+        #     target_position = [1, 0.3, -0.5]
+        #     print("Looking at hand")
+        #     tracker.lookAt(target_position, frame, speed, use_whole_body)
+        #     fillers = ["Mm-hmm", "uh huh", "okay", "I see"]
+        #     speech = animated_speech.say("^start(animations/Stand/Gestures/Explain_11) " + random.choice(fillers))
+            
+        # else:
+        #     if confidence < 0.8:
+        #         target_position = [sound_x, sound_y, sound_z]
+        #         print("Target Pos:", target_position)
+        #         tracker.lookAt(target_position, frame, speed, use_whole_body)
+
+
+        # have the NAO speak ChatGPT's response
+        if text != "":
+            if text != text_old:
+                animated_speech.say(text)
+                print(text)
+                text_old = text
+                with open("listen.txt", "w") as f:
+                    f.write("yes")
+                
+        time.sleep(1)
+    except Exception as e:
+        print("An error occurred: ", e)
+        time.sleep(1)
---
+++ speechOpenAI.py
@@ -0,0 +1,85 @@
+import time
+import multiprocessing
+import speech_recognition as sr
+from openai import OpenAI
+import json
+import os
+
+
+# code to figure out the microphone indexes for multi-microphone use
+'''
+microphones = sr.Microphone.list_microphone_names()
+for index, name in enumerate(microphones):
+  print(f"Microphone with index {index} and name \"{name}\" found")
+'''
+
+openAIKey = os.environ["OPENAI_API_KEY"]
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Initialize the OpenAI client
+client = OpenAI(api_key=openAIKey)
+MODEL = "gpt-4"
+
+# chat_history = [{"role": "system", "content": "You are a NAO robot that is in a pediatrician setting, attempting to make children more comfortable during their doctor visit. Respond as if you are an adult talking to a child. Use appropriate gestures as needed, and provide the response in this example format: First part of response ^start(animations/Stand/Gestures/Hey_1) second part of response."}]
+chat_history = [{"role": "system", "content": "You are a NAO robot that is in a pediatrician setting, attempting to make children more comfortable during their doctor visit. Respond as if you are an adult talking to a child. Keep responses under 100 words. Minimize the use of gestures in your responses, by adding ^mode(disabled) at the beginning of some of the second response. For example: ^mode(disabled) Hello there! How are you today?"}]
+with open("history.txt", "w") as f:
+	json.dump(chat_history,f)
+	
+def speak(mic,person):
+	while True:
+		with sr.Microphone(device_index=mic) as source:
+
+			r.adjust_for_ambient_noise(source)
+			
+			print("Listening...")
+			audio = r.listen(source)
+			print("Stop Listening")
+			
+			try:
+				# using google to transcribe the audio file to text
+				text = r.recognize_google(audio,language="en-US")
+				print("mic " + str(mic) + " " + person + " said: " + text)
+
+				# read current chat history
+				with open("history.txt", "r") as f:
+					chat_history = json.load(f)
+
+				# keeps the chat history with ChatGPT
+				chat_history.append({'role': 'user', 'content': text})
+				completion = client.chat.completions.create(
+					model= MODEL,
+					messages= chat_history
+				)
+				response = completion.choices[0].message.content
+				print("Assistant: " + response)
+
+				# Add the assistant's response to the chat history
+				chat_history.append({"role": "assistant", "content": response})
+
+				# Save the updated chat history back to the file
+				with open("history.txt", "w") as f:
+					json.dump(chat_history, f)
+
+				with open("response.txt", "w") as f:
+					f.write(response)
+
+				while True:
+					with open("listen.txt", "r") as f:
+						result = f.read()
+
+					if result == "yes":
+						with open("listen.txt", "w") as f:
+							f.write("no")
+						break
+
+			except Exception as e:
+				# print(f"An error occurred: {e}")
+				pass
+
+		# time.sleep(1)		
+		
+
+# replace the parameters accordingly
+speak(1,"Human")
